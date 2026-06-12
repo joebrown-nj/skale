1da979d3340f63c4e7ef04c39cfff588abd06719
jbrownwebsites <joebro84@yahoo.com>
2026-06-12 10:41:21 -0400

    new google tag manager for ppc ads

--- assets/js/main.js
+++ assets/js/main.js
@@ -1161,3 +1161,8 @@ function initializeStatsCounter() {
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-MGKXRNV7');
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-1029303333');
