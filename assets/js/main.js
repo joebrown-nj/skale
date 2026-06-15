c18a26636865902d23ef3eee17b59252af3e9867
jbrownwebsites <joebro84@yahoo.com>
2026-06-15 16:00:15 -0400

    consolidate google code

--- assets/js/main.js
+++ assets/js/main.js
@@ -989,12 +989,6 @@ $(window).on('load', function handleWindowLoad() {
     refreshAos(200);
 });
 
-window.dataLayer = window.dataLayer || [];
-
-function gtag() {
-    dataLayer.push(arguments);
-}
-
 //Get the button.
 const backToTopButton = document.getElementById('btn-back-to-top');
 
@@ -1151,18 +1145,3 @@ function initializeStatsCounter() {
     });
 }
 
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MGKXRNV7');
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'AW-1029303333');
-
-// gtag('js', new Date());
-// gtag('config', 'G-5HMT5HBM1Y', {
-//     cookie_flags: 'secure;samesite=none',
-// });
